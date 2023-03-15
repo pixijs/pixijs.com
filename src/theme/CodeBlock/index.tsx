1c7e3a217b279b93b9e96bde67515ac94935ee18
Alex Prokop <alex.prokop@play.co>
2023-03-15 18:01:32 +0000

    add dynamic code source CodeBlock

--- src/theme/CodeBlock/index.tsx
+++ src/theme/CodeBlock/index.tsx
@@ -1,21 +1,48 @@
-import CodeBlock from '@theme-original/CodeBlock';
-
-import PixiPlayground from '../../components/PixiPlayground';
-import styles from './index.module.css';
+import { useMemo } from 'react';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 
-export default function CodeBlockWrapper(props: { playground: boolean; children: string }): JSX.Element
+import CodeBlock from '@theme-original/CodeBlock';
+import PixiPlayground from '@site/src/components/PixiPlayground';
+
+import { examplesSource } from '@site/src/data/examples';
+import styles from './index.module.css';
+
+type ExamplesHash = typeof examplesSource;
+
+type CodeBlockWrapperProps = { [key: string]: any } & {
+    playground?: boolean;
+    children: string;
+};
+
+type HashType = Record<string, any>;
+type ExampleSourceLevelType = HashType | string | undefined;
+type ExampleSourceType = string | undefined;
+
+function getExampleSource(pathString: string, object: ExamplesHash): ExampleSourceType
+{
+    const path = pathString.split('.');
+
+    return path.reduce(
+        (objectLevel: ExampleSourceLevelType, levelKey) =>
+            (objectLevel ? ((objectLevel as HashType)[levelKey] as ExampleSourceLevelType) : objectLevel),
+        object,
+    ) as ExampleSourceType;
+}
+
+export default function CodeBlockWrapper({ children, playground = false, ...rest }: CodeBlockWrapperProps)
 {
     const isBrowser = useIsBrowser();
+    const exampleContent = useMemo(() =>
+        getExampleSource(children.trim(), examplesSource) || children, [children]);
 
-    if (props.playground && isBrowser)
+    if (playground && isBrowser)
     {
         return (
             <div className={styles.playgroundCodeBlock}>
-                <PixiPlayground code={props.children} />
+                <PixiPlayground code={exampleContent} />
             </div>
         );
     }
 
-    return <CodeBlock {...props} />;
+    return <CodeBlock {...rest}>{exampleContent}</CodeBlock>;
 }
