cd13c86bf193ffee8f4d6f11a93d80e53435f9cb
Alex Prokop <alex.prokop@play.co>
2023-03-16 12:00:17 +0000

    md js extraction script

--- scripts/extractExampleJs.js
+++ scripts/extractExampleJs.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { resolve } = require('path');
 const { readdir } = require('fs').promises;
+const { mkdirSync, readFileSync, writeFileSync } = require('fs');
 
 const ROOT = resolve(__dirname, '..');
 const EXAMPLES_MD_PATH = resolve(ROOT, 'docs', 'examples');
@@ -35,16 +36,56 @@ async function go()
             const relativePath = filePath.slice(EXAMPLES_MD_PATH.length + 1).slice(0, -3);
             const pathParts = relativePath.split('/').map((part) => snakeToCamel(part));
             const [directory, file] = pathParts;
+            const jsFile = `${file}.js`;
 
             return {
-                directory,
-                file,
+                sourcePath: filePath,
                 pathKey: pathParts.join('.'),
-                filePath,
+                directory,
+                variable: file,
+                file: jsFile,
+                destinationDirectory: [EXAMPLES_JS_PATH, directory].join('/'),
+                destinationPath: [EXAMPLES_JS_PATH, directory, jsFile].join('/'),
             };
         });
 
-    fileData.forEach((data) => {});
+    const importLines = fileData.map(
+        ({ directory, file, variable }) => `import ${variable} from '!!raw-loader!./${directory}/${file}'`,
+    );
+    const exportLines = ['export examplesSource = {', ...fileData.map(({ variable }) => `    ${variable}`), `};`];
+    const indexSource = [...importLines, '', ...exportLines, ''].join('\n');
+
+    writeFileSync(`${EXAMPLES_JS_PATH}/index.ts`, indexSource, 'utf8');
+
+    fileData.forEach(({ sourcePath, pathKey, destinationDirectory, destinationPath }) =>
+    {
+        const mdSource = readFileSync(sourcePath, 'utf8');
+        const mdSourceLines = mdSource.split('\n');
+        const startIndex = mdSourceLines.findIndex((line) => line.trim() === '```js playground');
+        const endIndex = mdSourceLines.findIndex((line) => line.trim() === '```');
+
+        if (startIndex === -1 || endIndex === -1)
+        {
+            console.log('No example', sourcePath);
+
+            return;
+        }
+
+        const nextMdSource = [
+            ...mdSourceLines.slice(0, startIndex + 1),
+            `${pathKey}`,
+            ...mdSourceLines.slice(endIndex),
+        ].join('\n');
+        const jsSource = mdSourceLines.slice(startIndex + 1, endIndex).join('\n');
+
+        // overwrite source file with new contents
+        writeFileSync(sourcePath, nextMdSource, 'utf8');
+
+        // ensure output dir exists
+        mkdirSync(destinationDirectory, { recursive: true });
+
+        writeFileSync(destinationPath, jsSource, 'utf8');
+    });
 }
 
 go();
