dc28e4a3c6ac6f5af2011acc2673ead1c31704e2
thalys <thalys.segala@play.co>
2023-03-21 00:18:06 -0300

    Features animations

--- src/components/Homepage/KeyOfferings/index.tsx
+++ src/components/Homepage/KeyOfferings/index.tsx
@@ -1,20 +1,32 @@
 import styles from './index.module.scss';
 import content from './content.json';
 import graphics from './graphics.json';
+import { useInView } from 'react-intersection-observer';
+
+const Features = (): JSX.Element =>
+{
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+    });
+
+    return (
+        <div ref={ref} className={`container flex ${inView ? 'animate-in' : ''}`}>
+            {content.map((item, index) => (
+                <div key={index} className="col col--4 padding-vert--md">
+                    <span>{`0${index + 1}`}</span>
+                    <h2 className="underline">{item.heading}</h2>
+                    <p>{item.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default function KeyOfferings(): JSX.Element
 {
     return (
-        <div className={`padding-vert--lg ${styles.highlights}`}>
-            <div className="container flex">
-                {content.map((item, index) => (
-                    <div key={index} className="col col--4 padding-vert--md">
-                        <span>{`0${index + 1}`}</span>
-                        <h2 className="underline">{item.heading}</h2>
-                        <p>{item.description}</p>
-                    </div>
-                ))}
-            </div>
+        <div className={`padding-vert--lg ${styles.highlights} features`}>
+            <Features />
             <div className={styles.devices}>
                 {graphics.devices.map((device, index) => (
                     <div
