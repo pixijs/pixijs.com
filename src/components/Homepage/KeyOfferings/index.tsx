30c421bf82cd9aafadc4c7817980a383f656c1e2
thalys <thalys.segala@play.co>
2023-03-22 10:53:24 -0300

    Fix timings

--- src/components/Homepage/KeyOfferings/index.tsx
+++ src/components/Homepage/KeyOfferings/index.tsx
@@ -11,22 +11,28 @@ const Offerings = (): JSX.Element =>
 
     return (
         <div ref={ref} className={`container flex`}>
-            {content.map((item, index) => (
-                <div
-                    key={index}
-                    className="col col--4 padding-vert--md"
-                    style={{
-                        transform: 'translateX(100vw)',
-                        animation: inView
-                            ? `offering-in 1s cubic-bezier(0.5, 1, 0.5, 1) ${Number(0.3 * index)}s forwards`
-                            : '',
-                    }}
-                >
-                    <span>{`0${index + 1}`}</span>
-                    <h2 className="underline">{item.heading}</h2>
-                    <p>{item.description}</p>
-                </div>
-            ))}
+            {content.map((item, index) =>
+            {
+                if (!inView)
+                {
+                    return null;
+                }
+
+                return (
+                    <div
+                        key={index}
+                        className="col col--4 padding-vert--md"
+                        style={{
+                            transform: 'translateX(100vw)',
+                            animation: `offering-in 1s cubic-bezier(0.5, 1, 0.5, 1) ${Number(0.15 * index)}s forwards`,
+                        }}
+                    >
+                        <span>{`0${index + 1}`}</span>
+                        <h2 className="underline">{item.heading}</h2>
+                        <p>{item.description}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 };
@@ -41,7 +47,6 @@ const Devices = (): JSX.Element =>
         <div ref={ref} className={styles.devices}>
             {graphics.devices.map((device, index) =>
             {
-                const duration = 0.3 * (index + 1);
                 const delay = device.styles.dataDelay;
 
                 if (!inView)
@@ -55,7 +60,7 @@ const Devices = (): JSX.Element =>
                         style={{
                             ...device.styles,
                             position: 'absolute',
-                            animation: `device-in ${duration}s cubic-bezier(0.3, 1, 0.5, 1) ${delay}s forwards`,
+                            animation: `device-in 0.4s cubic-bezier(0.3, 1, 0.5, 1) ${delay}s forwards`,
                         }}
                     >
                         <img src={device.img} alt={device.alt} />
