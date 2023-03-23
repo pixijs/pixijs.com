a31c4fd122825bc5bf3884332acc0be1989d1523
cece-playco <cece.phu@play.co>
2023-03-23 15:35:51 -0400

    Change font color, add max width

--- src/components/Homepage/OpenCollective/index.tsx
+++ src/components/Homepage/OpenCollective/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import styles from './index.module.scss';
-import useWidget from './openCollectiveWidget';
 
 export default function ClosingSection(): JSX.Element
 {
-    useWidget();
+    useEffect(() =>
+    {
+        const body = document.getElementById('openCollectiveWidget');
+
+        const script = document.createElement('script');
+
+        script.setAttribute('id', 'iframeWidget');
+        script.setAttribute('src', `https://opencollective.com/pixijs/banner.js?style={"h2":{"color":"gray"}}`);
+
+        body?.appendChild(script);
+
+        return () =>
+        {
+            script?.remove();
+        };
+    });
 
     return <div id="openCollectiveWidget" className={`${styles.wrapper}`}></div>;
 }
