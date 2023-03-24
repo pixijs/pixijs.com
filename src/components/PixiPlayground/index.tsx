a5e54d87577e6f8926c725fa726495808bf226d1
Alex Prokop <alex.prokop@play.co>
2023-03-24 15:55:50 +0000

    * tweak sandpack configuration for best combination of vanilla template + babel config + open in codesandbox functionality, encapsulate in useSandpackConfiguration hook * make playground example styling hack a little more reusable/less magic

--- src/components/PixiPlayground/index.tsx
+++ src/components/PixiPlayground/index.tsx
@@ -1,12 +1,14 @@
 import { useColorMode } from '@docusaurus/theme-common';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import classNames from 'classnames';
 import { SandpackLayout, SandpackPreview, SandpackProvider, useActiveCode, useSandpack } from '@codesandbox/sandpack-react';
+import { useContainerClassNameModifier } from '@site/src/hooks/useContainerClassNameModifier';
+import { latestVersion } from './usePixiVersions';
 import MonacoEditor from './MonacoEditor';
+import { useSandpackConfiguration } from './useSandpackConfiguration';
 import type { CodeChangeCallbackType } from './MonacoEditor';
 
 import styles from './index.module.scss';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 type PlaygroundMode = 'tutorial' | 'fullscreen' | 'example';
 
@@ -33,10 +35,10 @@ function Playground({ mode, onCodeChanged }: PlaygroundProps)
         [onCodeChanged],
     );
 
-    const handleToggle = () =>
+    const handleToggle = useCallback(() =>
     {
-        setShowOutput(!showOutput);
-    };
+        setShowOutput((lastShowOutput) => !lastShowOutput);
+    }, []);
 
     // TODO: we don't change the value of activeFile so why the key?
     return (
@@ -46,8 +48,8 @@ function Playground({ mode, onCodeChanged }: PlaygroundProps)
             </div>
 
             <div className={styles.previewWrapper}>
-                <SandpackPreview showOpenInCodeSandbox={true} className={styles.sandpackPreview} />
-                {bundlerState === null && <div className={styles.sandpackLoadingOverlay}></div>}
+                <SandpackPreview showOpenInCodeSandbox={true} />
+                {!bundlerState && <div className={styles.sandpackLoadingOverlay}></div>}
             </div>
 
             <button onClick={handleToggle}>{showOutput ? 'Show Code' : 'Show Output'}</button>
@@ -57,52 +59,46 @@ function Playground({ mode, onCodeChanged }: PlaygroundProps)
 
 type PixiPlaygroundProps = {
     code: string;
+    isPixiWebWorkerVersion?: boolean;
+    isPixiDevVersion?: boolean;
+    pixiVersion?: string;
     mode?: PlaygroundMode;
     onCodeChanged?: CodeChangeCallbackType;
 };
 
-export default function PixiPlayground({ code, onCodeChanged, mode = 'example' }: PixiPlaygroundProps)
+export default function PixiPlayground({
+    code,
+    onCodeChanged,
+    isPixiWebWorkerVersion = false,
+    isPixiDevVersion = false,
+    pixiVersion = latestVersion,
+    mode = 'example',
+}: PixiPlaygroundProps)
 {
-    const { siteConfig } = useDocusaurusContext();
     const { colorMode } = useColorMode();
 
+    const { key, files, customSetup } = useSandpackConfiguration({
+        code,
+        isPixiDevVersion,
+        isPixiWebWorkerVersion,
+        pixiVersion,
+    });
+
     // Hack to make the examples pages full width on wide screens
-    // eslint-disable-next-line consistent-return
-    useEffect(() =>
-    {
-        const mainContainer = document.querySelector<HTMLDivElement>('main .container');
-
-        if (mode === 'example' && mainContainer !== null)
-        {
-            mainContainer.style.maxWidth = '100%';
-
-            return () =>
-            {
-                mainContainer.style.maxWidth = '';
-            };
-        }
-    }, [mode]);
+    useContainerClassNameModifier('example', mode === 'example');
 
     return (
         <SandpackProvider
+            key={key}
             template="vanilla"
             theme={colorMode}
-            files={{
-                '/src/index.js': code,
-            }}
-            customSetup={{}}
+            files={files}
+            customSetup={customSetup}
             options={{
                 classes: {
                     'sp-wrapper': styles.spWrapper,
                     'sp-layout': styles.spLayout,
                 },
-                externalResources: [
-                    'https://beta.pixijs.com/playground.css',
-                    `https://pixijs.download/${siteConfig.customFields?.PIXI_VERSION}/pixi.min.js`,
-                    `https://pixijs.download/${siteConfig.customFields?.PIXI_VERSION}/packages/graphics-extras.js`,
-                    `https://pixijs.download/${siteConfig.customFields?.PIXI_VERSION}/packages/math-extras.js`,
-                    `https://pixijs.download/${siteConfig.customFields?.PIXI_VERSION}/packages/webworker.js`,
-                ],
             }}
         >
             <Playground mode={mode} onCodeChanged={onCodeChanged} />
