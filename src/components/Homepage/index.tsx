ac87bdf492876c550f9717833b2966f3554c58cb
Matt Karl <matt@mattkarl.com>
2023-07-28 10:04:56 -0400

    Update src/components/Homepage/index.tsx

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -13,9 +13,9 @@ export default function Homepage(): JSX.Element
     return (
         <Layout
             title={`${siteConfig.title} | The HTML5 Creation Engine`}
-            description={"PixiJS - The HTML5 Creation Engine. " +
-            "Create beautiful digital content with the fastest, " +
-            "most flexible 2D WebGL renderer."}
+            description={'PixiJS - The HTML5 Creation Engine. '
+            + 'Create beautiful digital content with the fastest, '
+            + 'most flexible 2D WebGL renderer.'}
         >
             <main>
                 <div className="text--center">
