9f4e8aed029bad9b9a9ac07eda18b1cdfb175ecd
aduros-alt <1450489+aduros-alt@users.noreply.github.com>
2024-01-19 08:34:17 -0300

    Add support for editting multiple files in Playground component. (#67)
    * Add support for editting multiple files in Playground component.
    
    Follow up work is needed to refactor the examples to use multiple files.
    
    * Fix.

--- src/components/PixiPlayground/MonacoEditor.tsx
+++ src/components/PixiPlayground/MonacoEditor.tsx
@@ -2,17 +2,15 @@ import { useCallback, useEffect, useRef } from 'react';
 import type { editor } from 'monaco-editor';
 import { useColorMode } from '@docusaurus/theme-common';
 import Editor from '@monaco-editor/react';
-
-const ROOT_DIR = 'inmemory://model/';
+import { FileTabs, SandpackStack, useActiveCode, useSandpack } from '@codesandbox/sandpack-react';
 
 export type CodeChangeCallbackType = (code: string | undefined) => void;
 
 type MonacoEditorProps = {
-    code: string;
-    onChange: CodeChangeCallbackType;
+    onChange?: CodeChangeCallbackType;
 };
 
-export default function MonacoEditor({ code, onChange }: MonacoEditorProps)
+export default function MonacoEditor({ onChange }: MonacoEditorProps)
 {
     const editorRef = useRef(null);
 
@@ -52,16 +50,25 @@ export default function MonacoEditor({ code, onChange }: MonacoEditorProps)
     };
 
     const { colorMode } = useColorMode();
+    const { code, updateCode } = useActiveCode();
+    const { sandpack } = useSandpack();
 
     return (
-        <Editor
-            defaultLanguage="javascript"
-            value={code}
-            defaultPath={`${ROOT_DIR}/src/index.ts`}
-            onChange={onChange}
-            options={options}
-            onMount={handleEditorDidMount}
-            theme={colorMode === 'dark' ? 'vs-dark' : 'light'}
-        />
+        <SandpackStack style={{ height: '100%', margin: 0 }}>
+            <FileTabs />
+            <Editor
+                key={sandpack.activeFile}
+                defaultLanguage="javascript"
+                defaultValue={code}
+                options={options}
+                onMount={handleEditorDidMount}
+                onChange={(value) =>
+                {
+                    updateCode(value || '');
+                    onChange?.(value);
+                }}
+                theme={colorMode === 'dark' ? 'vs-dark' : 'light'}
+            />
+        </SandpackStack>
     );
 }
