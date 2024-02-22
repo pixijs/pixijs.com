2e5d19cae9ac3329f50339f13dfd66c65ea9d0bb
Zyie <24736175+Zyie@users.noreply.github.com>
2024-02-22 13:38:45 +0000

    undo versioning

--- src/pages/versions.tsx
+++ src/pages/versions.tsx
@@ -143,41 +143,6 @@ export default function Version(): JSX.Element
                     </div>
                 )}
 
-                {latestVersion && (
-                    <div className="margin-bottom--lg">
-                        <Heading as="h3" id="next">
-                            <Translate id="versionsPage.current.title">Current version (Stable)</Translate>
-                        </Heading>
-                        <p>
-                            <Translate id="versionsPage.current.description">
-                                Here you can find the documentation for current released version.
-                            </Translate>
-                        </p>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th>{latestVersion.version}</th>
-                                    <td>
-                                        <Link to={latestVersion.docs}>
-                                            <DocumentationLabel />
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <Link to={latestVersion.build}>
-                                            <BuildLabel />
-                                        </Link>
-                                    </td>
-                                    <td>
-                                        <Link to={latestVersion.releaseNotes}>
-                                            <ReleaseNotesLabel />
-                                        </Link>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                )}
-
                 {pastVersions.length > 0 && (
                     <div className="margin-bottom--lg">
                         <Heading as="h3" id="archive">
