2c6482aa0373097955df191cf35abca98d1bdfde
Zyie <24736175+Zyie@users.noreply.github.com>
2025-01-07 09:13:20 +0000

    feat: editor updates (#149)

--- src/theme/CodeBlock/index.tsx
+++ src/theme/CodeBlock/index.tsx
@@ -1,6 +1,3 @@
-import styles from './index.module.css';
-import useIsBrowser from '@docusaurus/useIsBrowser';
-import PixiPlayground from '@site/src/components/Playground/PixiPlayground';
 import CodeBlock from '@theme-original/CodeBlock';
 
 type CodeBlockWrapperProps = { [key: string]: any } & {
@@ -9,23 +6,7 @@ type CodeBlockWrapperProps = { [key: string]: any } & {
     children: string;
 };
 
-export default function CodeBlockWrapper({
-    children,
-    playground = false,
-    usesWebWorkerLibrary = false,
-    ...rest
-}: CodeBlockWrapperProps)
+export default function CodeBlockWrapper({ children, ...rest }: CodeBlockWrapperProps)
 {
-    const isBrowser = useIsBrowser();
-
-    if (playground && isBrowser)
-    {
-        return (
-            <div className={styles.playgroundCodeBlock}>
-                <PixiPlayground code={children} isPixiWebWorkerVersion={usesWebWorkerLibrary} />
-            </div>
-        );
-    }
-
     return <CodeBlock {...rest}>{children}</CodeBlock>;
 }
